6f838f47f20c6e63337687c67eb489ecb54ba26f
NXLuan <xuanluan1412@gmail.com>
2021-09-30 16:22:52 +0700

    set url

--- Estant-Backend/Estant.API/Program.cs
+++ Estant-Backend/Estant.API/Program.cs
@@ -20,7 +20,7 @@ namespace Estant.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseUrls("https://*:5001");
                 });
     }
 }
